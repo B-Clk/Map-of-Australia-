f24ac5319fee1e84576511ee5a9761de87ac19f4
B-Clk <163749753+B-Clk@users.noreply.github.com>
2024-11-19 00:42:30 +0300

    added a button

--- myflutterapp/lib/main.dart
+++ myflutterapp/lib/main.dart
@@ -14,21 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -39,13 +24,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
   final String title;
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -54,6 +32,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   late MapShapeSource _shapeSource;
   late List<MapModel> _mapData;
+  int _index = 8;
+  bool isPressed = false;
   @override
   void initState() {
     _mapData = _getMapData();
@@ -102,6 +82,10 @@ class _MyHomePageState extends State<MyHomePage> {
                     legend: MapLegend(MapElement
                         .shape), //this shows the label color for each one.
                     shapeTooltipBuilder: (BuildContext context, int index) {
+                      setState(() {
+                        _index = index;
+                        isPressed = true;
+                      });
                       return Padding(
                         padding: EdgeInsets.all(7),
                         child: Text(
@@ -118,6 +102,21 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
           ),
+          Padding(
+              padding: EdgeInsets.all(10),
+              child: isPressed
+                  ? ElevatedButton(
+                      child: Text(_mapData[_index].stateCode),
+                      onPressed: () {
+                        setState(() {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => CityDetailPage()));
+                        });
+                      },
+                    )
+                  : Text("Şehir seçmek için tıklayın!"))
         ],
       ),
     );
